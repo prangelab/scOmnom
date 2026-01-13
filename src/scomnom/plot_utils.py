162aff9e9f8a9fb2987f418b7d1e6b42a8f9a352
koenprange <koenpragne@mac.com>
2026-01-13 14:10:39 +0100

    Started changelog

--- src/scomnom/plot_utils.py
+++ src/scomnom/plot_utils.py
@@ -7,6 +7,7 @@ import logging
 from sklearn.metrics import silhouette_samples
 
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 import scanpy as sc
 import pandas as pd
