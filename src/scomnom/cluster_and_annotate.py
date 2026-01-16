6abc14cdaf26e8d89b10148ed611f882d8ef2544
koenprange <koenpragne@mac.com>
2026-01-16 21:41:22 +0100

    Optimising cluster-and-annotate. refactored cluster_adn_annotate. moved a lot of funcitons to new *util.py files, for clarity.

--- src/scomnom/cluster_and_annotate.py
+++ src/scomnom/cluster_and_annotate.py
@@ -4,11 +4,9 @@ from __future__ import annotations
 import json
 import logging
 from pathlib import Path
-from typing import Optional
 
 import anndata as ad
 import numpy as np
-import pandas as pd
 import scanpy as sc
 
 from .config import ClusterAnnotateConfig
